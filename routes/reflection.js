4322eeb2ea9acf332968b2056eaaefd39d1d2aa4
HafidurR <deifah126@gmail.com>
2021-11-22 10:44:32 +0700

    edit routes

--- routes/reflection.js
+++ routes/reflection.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const reflections  = require('../controller/reflections');
 
 router.post('/', reflections.createReflections);
+router.put('/:id', reflections.updateReflections);
 
 module.exports = router;
