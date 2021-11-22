02a770178d594a746720526051515b2e8b621ca8
Teuku Raja <teramuza@gmail.com>
2021-11-22 15:48:56 +0700

    create response utils

---
+++ helper/response.utils.js
@@ -0,0 +1,36 @@
+const successResponse = (res, data, message) => {
+    return res.status(200).json({
+        status: 'success',
+        message,
+        data
+    })
+}
+
+const unauthorizedResponse = (res, message) => {
+    return res.status(403).json({
+        status: 'unauthorized',
+        message
+    })
+}
+
+const badRequestResponse = (res, message, data) => {
+    return res.status(400).json({
+        status: 'bad request',
+        message,
+        data
+    })
+}
+
+const serverErrorResponse = (res, message) => {
+    return res.status(503).json({
+        status: 'server error',
+        message
+    })
+}
+
+module.exports = {
+    successResponse,
+    unauthorizedResponse,
+    badRequestResponse,
+    serverErrorResponse
+}
