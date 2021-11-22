a44d77696287a94228fd28c659f4efaa59ce961b
Teuku Raja <teramuza@gmail.com>
2021-11-22 15:34:47 +0700

    add try catch to prevent crash during service running

--- controller/login.js
+++ controller/login.js
@@ -3,49 +3,53 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 module.exports = async (req, res) => {
-  const {
-    email,
-    password
-  } = req.body
+  try {
+    const {
+      email,
+      password
+    } = req.body
 
-  
-  db.query("SELECT * FROM users WHERE email = '" + email + "' ", (err, result) => {
-    if (err) {
-      return res.status(400).json({
-        status: 'error',
-        message: err.message,
-      })
-    }
 
-    if (result.rowCount != 0) {
-      const data = {
-        id: result.rows[0].id,
-        name: result.rows[0].name,
-        email: result.rows[0].email,
-      }
-
-      const isValidPassword = bcrypt.compare(password, result.rows[0].password);
-      if (isValidPassword) {
-        let token = jwt.sign(data, 'aff4d19955f3afe4a1ec122e969750d09c77510cdb9eca85df4335663563', {
-          expiresIn: '1h'
-        })
-        res.status(200).json({
-          status: 'success',
-          message: 'success login',
-          token
-        })
-      } else {
+    db.query("SELECT * FROM users WHERE email = '" + email + "' ", (err, result) => {
+      if (err) {
         return res.status(400).json({
           status: 'error',
-          message: 'email or password incorrect'
+          message: err.message,
         })
       }
-    } else {
-      return res.status(404).json({
-        status: 'Not found',
-        message: "Data tidak ditemukan",
-      })
-    }
 
-  })
+      if (result.rowCount != 0) {
+        const data = {
+          id: result.rows[0].id,
+          name: result.rows[0].name,
+          email: result.rows[0].email,
+        }
+
+        const isValidPassword = bcrypt.compare(password, result.rows[0].password);
+        if (isValidPassword) {
+          let token = jwt.sign(data, 'aff4d19955f3afe4a1ec122e969750d09c77510cdb9eca85df4335663563', {
+            expiresIn: '1h'
+          })
+          res.status(200).json({
+            status: 'success',
+            message: 'success login',
+            token
+          })
+        } else {
+          return res.status(400).json({
+            status: 'error',
+            message: 'email or password incorrect'
+          })
+        }
+      } else {
+        return res.status(404).json({
+          status: 'Not found',
+          message: "Data tidak ditemukan",
+        })
+      }
+
+    })
+  } catch (e) {
+    res.status(503).send(e.message);
+  }
 }
--- controller/register.js
+++ controller/register.js
@@ -2,27 +2,31 @@ const db = require('../config/db');
 const bcrypt = require('bcrypt');
 
 module.exports = async (req, res) => {
-  const {
-    name,
-    email,
-    password
-  } = req.body;
+  try {
+    const {
+      name,
+      email,
+      password
+    } = req.body;
 
-  await bcrypt.hash(password, 10, (err, encrypt) => {
-    db.query(`INSERT INTO users (name, email, password) VALUES ('${name}', '${email}', '${encrypt}') `, (err, result) => {
-      if (err) {
-        return res.status(400).json({
-          status: 'error',
-          message: err.message
-        })
-      } else {
-        return res.status(201).json({
-          status: 'success',
-          message: 'success add data',
-          data: result.rows
-        })
-      }
-    })
-  });
-
+    await bcrypt.hash(password, 10, (err, encrypt) => {
+      db.query(`INSERT INTO users (name, email, password)
+                VALUES ('${name}', '${email}', '${encrypt}') `, (err, result) => {
+        if (err) {
+          return res.status(400).json({
+            status: 'error',
+            message: err.message
+          })
+        } else {
+          return res.status(201).json({
+            status: 'success',
+            message: 'success add data',
+            data: result.rows
+          })
+        }
+      })
+    });
+  } catch (e) {
+    res.status(503).send(e.message);
+  }
 }
