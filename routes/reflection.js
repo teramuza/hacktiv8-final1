3654dbdbfd85e9efcbdbfefcc2ba8db523746135
Teuku Raja <teramuza@gmail.com>
2021-11-22 10:32:44 +0700

    fix date time format & use middleware verify token

--- routes/reflection.js
+++ routes/reflection.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const reflections  = require('../controller/reflections');
+const verifyToken = require('../middleware/verifyToken');
 
-router.post('/', reflections.createReflections);
+router.post('/', verifyToken, reflections.createReflections);
 
 module.exports = router;
