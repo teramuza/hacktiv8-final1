ba2cf6799d0afb656b375685455d3b5fa595bbec
Teuku Raja <teramuza@gmail.com>
2021-11-22 16:17:41 +0700

    implement response utils to verifyToken middleware

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken')
+const response = require('../helper/response.utils')
 
 module.exports = async (req, res, next) => {
   const token = req.headers['x-access-token']
+  if (!token)
+    return response.forbiddenResponse(res, 'access token required');
 
   jwt.verify(token, 'aff4d19955f3afe4a1ec122e969750d09c77510cdb9eca85df4335663563', function (err, decoded) {
-    if (err) {
-      return res.status(403).json({
-        message: err.message
-      })
-    }
+    if (err)
+      return response.unauthorizedResponse(res, err.message);
 
     req.user = decoded;
     return next();
