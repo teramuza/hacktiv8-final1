00f17c39cdea67661233e69076afc591ae3898e6
Teuku Raja <teramuza@gmail.com>
2021-11-22 16:36:17 +0700

    move jwt key secret to env

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,12 +1,15 @@
 const jwt = require('jsonwebtoken')
 const response = require('../helper/response.utils')
+require('dotenv').config();
+
+const {JWT_SECRET_KEY} = process.env;
 
 module.exports = async (req, res, next) => {
   const token = req.headers['x-access-token']
   if (!token)
     return response.forbiddenResponse(res, 'access token required');
 
-  jwt.verify(token, 'aff4d19955f3afe4a1ec122e969750d09c77510cdb9eca85df4335663563', function (err, decoded) {
+  jwt.verify(token, JWT_SECRET_KEY, function (err, decoded) {
     if (err)
       return response.unauthorizedResponse(res, err.message);
 
